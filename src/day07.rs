a23f3b4ea5a8ccce7c61b19d311f5b06b0f43927
Hugo Tunius <h@tunius.se>
2021-12-07 19:31:38 +0000

    Solve day 07

--- src/day07.rs
+++ src/day07.rs
@@ -1,22 +1,53 @@
+use crate::parse_custom_separated;
+
+fn best_fuel_cost<F>(positions: &[i64], mut calc: F) -> i64
+where
+    F: FnMut(i64) -> i64,
+{
+    let (max, min) = (
+        *positions.iter().max().unwrap(),
+        *positions.iter().min().unwrap(),
+    );
+
+    let (_, fuel) = (min..=max)
+        .map(|o| (o, positions.iter().map(|p| calc((p - o).abs())).sum()))
+        .min_by_key(|(_, fuel)| *fuel)
+        .unwrap();
+
+    fuel
+}
+
+fn identity<T>(x: T) -> T {
+    x
+}
+
 pub fn star_one(input: &str) -> i64 {
-    0
+    let positions: Vec<_> = parse_custom_separated::<i64>(input, ",").collect();
+
+    best_fuel_cost(&positions, identity)
 }
 
 pub fn star_two(input: &str) -> i64 {
-    0
+    let positions: Vec<_> = parse_custom_separated::<i64>(input, ",").collect();
+    fn fuel_cost(moves: i64) -> i64 {
+        (moves * (moves + 1)) / 2
+    }
+
+    best_fuel_cost(&positions, fuel_cost)
 }
 
 #[cfg(test)]
 mod tests {
     use super::{star_one, star_two};
+    const INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one(INPUT), 37);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two(INPUT), 168);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -245,8 +245,8 @@ mod tests {
 
         let input = load_file("day07.txt");
 
-        assert_eq!(star_one(&input), 1);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_one(&input), 356958);
+        assert_eq!(star_two(&input), 105461913);
     }
 
     #[test]
